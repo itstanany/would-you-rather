68014983c1ee60aee2ce2d5601caca6ce2e9c7ed
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2021-07-29 17:56:23 +0200

    setting questions redux reducer

---
+++ src/store/questions/reducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+  questions: {},
+  loading: false,
+  error: null,
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export { reducer }
