5d59f757583823de9314fbbd6cb4855b7f0c8e91
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2021-07-29 18:09:33 +0200

    adding redux-devtools

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,13 @@
-import { createStore, compose, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import { rootReducer } from './rootReducer';
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 const configureStore = (preLoadedState) => {
   const middleWares = [thunkMiddleware];
   const middlewareEnhancer = applyMiddleware(...middleWares);
 
-  const composedEnhancers = compose(middlewareEnhancer)
+  const composedEnhancers = composeWithDevTools(middlewareEnhancer)
   const store = createStore(rootReducer, preLoadedState, composedEnhancers);
   return store;
 }
