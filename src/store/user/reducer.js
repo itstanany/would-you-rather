eed257e25f52be6575a890950c4cf168f7f32e5d
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2021-07-30 09:57:28 +0200

    user login - logout basic reducer implementation

--- src/store/user/reducer.js
+++ src/store/user/reducer.js
@@ -1,3 +1,11 @@
+import { constants } from "./user.constants";
+
+const states = {
+  authenticated: 'authenticated',
+  unauthenticated: 'unauthenticated',
+  pending: 'pending',
+}
+
 const initialState = {
   user: {
     id: '',
@@ -8,17 +16,100 @@ const initialState = {
   },
   loading: false,
   error: null,
-  authenticate: false,
+  // authenticate: false,
+  state: states.unauthenticated,
 }
 
 
 const reducer = (state = initialState, action) => {
-  switch (action.type) {
+  let newState;
+  const { type = '', payload = {} } = action;
+  switch (state.state) {
+    case states.unauthenticated:
+      if (type === constants.USER_LOGIN_REQUEST) {
+        console.log('inside login request')
+        newState = {
+          ...state,
+          state: states.pending,
+          loading: true,
+        }
+        return newState;
+      }
+      break
+    case states.pending:
+      switch (type) {
+        case constants.USER_LOGIN_SUCCESS:
+          newState = {
+            ...state,
+            loading: false,
+            state: states.authenticated,
+            user: payload.user,
+          }
+          return newState;
+        case constants.USER_LOGIN_FAILURE:
+          newState = {
+            ...state,
+            loading: false,
+            state: states.unauthenticated,
+            error: payload.error
+          }
+          return newState;
+        case constants.USER_LOGOUT_SUCCESS:
+          newState = {
+            ...state,
+            loading: false,
+            state: states.unauthenticated,
+            user: initialState.user
+          };
+          return newState;
+        case constants.USER_LOGOUT_FAILURE:
+          newState = {
+            ...state,
+            loading: false,
+            error: payload.error,
+            state: states.authenticated,
+          }
+          return newState
+        default:
+          return state;
+      }
+
+    case states.authenticated:
+      if (type === constants.USER_LOGOUT_REQUEST) {
+        newState = {
+          ...state,
+          loading: true,
+          state: states.pending,
+        }
+        return newState
+      }
+      break
     default:
       return state;
   }
+  return state;
 }
 
 export {
   reducer
 }
+
+
+ // if (type === constants.USER_LOGIN_SUCCESS) {
+      //   newState = {
+      //     ...state,
+      //     loading: false,
+      //     state: states.authenticated,
+      //     user: payload.user,
+      //   }
+      //   return newState;
+      // }
+      // if (type === constants.USER_LOGIN_FAILURE) {
+      //   newState = {
+      //     ...state,
+      //     loading: false,
+      //     state: states.unauthenticated,
+      //     error: payload.error
+      //   }
+      //   return newState;
+      // }
