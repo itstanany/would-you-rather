4e8dc9e9946edbac054234c3ad77fde16d0a611f
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2021-07-31 17:30:17 +0200

    refactor eslint correction

--- src/components/QuestionDetail/index.js
+++ src/components/QuestionDetail/index.js
@@ -1,27 +1,31 @@
-import { useCallback, useEffect, useRef, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
-import { saveAnswer } from "../../store";
-import { questionsSelector, usersSelector, userSelector } from '../../utils'
-import { NotFound } from "../NotFound";
-import { QuestionDetailComponent } from "./QuestionDetail.component";
+import {
+  useCallback, useEffect, useRef, useState,
+} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { saveAnswer } from '../../store';
+import { questionsSelector, usersSelector, userSelector } from '../../utils';
+import { NotFound } from '../NotFound';
+import { QuestionDetailComponent } from './QuestionDetail.component';
 
 const QuestionDetail = () => {
-  const { id } = useParams();
+  const { question_id: id } = useParams();
   const { questions } = useSelector(questionsSelector);
   const question = useRef(questions[id]);
   const dispatch = useDispatch();
 
   const { users } = useSelector(usersSelector);
   const {
-    user: currentUser
+    user: currentUser,
   } = useSelector(userSelector);
   const [checkedItem, setCheckedItem] = useState('');
   const authorUser = useRef(users[question?.current?.author] || {});
-  const [totalVotes, setTotalVotes] = useState(question?.current?.optionOne?.votes?.length + question?.current?.optionTwo?.votes?.length);
+  const [totalVotes, setTotalVotes] = useState((
+    question?.current?.optionOne?.votes?.length + question?.current?.optionTwo?.votes?.length));
   useEffect(() => {
     question.current = questions[id];
-    setTotalVotes(question?.current?.optionOne?.votes?.length + question?.current?.optionTwo?.votes?.length)
+    setTotalVotes((
+      question?.current?.optionOne?.votes?.length + question?.current?.optionTwo?.votes?.length));
   }, [questions, id]);
   useEffect(() => {
     authorUser.current = users[question?.current?.author] || {};
@@ -33,28 +37,29 @@ const QuestionDetail = () => {
 
   const submitAnswer = useCallback((e) => {
     e.preventDefault();
-    dispatch(saveAnswer({ answer: checkedItem, qId: id }))
+    dispatch(saveAnswer({ answer: checkedItem, qId: id }));
   }, [checkedItem, id, dispatch]);
 
   return (
     question.current
-      ? <QuestionDetailComponent
-        question={question.current}
-        submitAnswer={submitAnswer}
-        handleChangeCheck={handleChangeCheck}
-        id={id}
-        currentUser={currentUser}
-        checkedItem={checkedItem}
-        user={authorUser.current}
-        totalVotes={totalVotes}
-      />
+      ? (
+        <QuestionDetailComponent
+          question={question.current}
+          submitAnswer={submitAnswer}
+          handleChangeCheck={handleChangeCheck}
+          id={id}
+          currentUser={currentUser}
+          checkedItem={checkedItem}
+          user={authorUser.current}
+          totalVotes={totalVotes}
+        />
+      )
       : <NotFound />
   );
-}
-
+};
 
 export default QuestionDetail;
 
 export {
   QuestionDetail,
-}
+};
