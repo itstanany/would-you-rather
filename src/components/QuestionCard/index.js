5accad8ae0fc8ccfd2acff9a0993ada0ff9fc946
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2021-07-31 17:29:16 +0200

    refactor eslint correction

--- src/components/QuestionCard/index.js
+++ src/components/QuestionCard/index.js
@@ -1,39 +1,39 @@
-import { Link } from "react-router-dom";
-import { Button, Item } from "semantic-ui-react"
+import { Link } from 'react-router-dom';
+import { Button, Item } from 'semantic-ui-react';
 
-const QuestionCard = ({ question, user }) => {
-  return (
-    <Item>
-      <Item.Image size='small' src={user.avatarURL} />
+const QuestionCard = ({ question, user }) => (
+  <Item>
+    <Item.Image size="small" src={user.avatarURL} />
 
-      <Item.Content>
-        <Item.Header as='a'>
-          {
-            user.name
-          }, asks Would You Rather &#63;
-        </Item.Header>
-        <Item.Description>
-          <ul>
-            <li>
-              {question.optionOne.text}
-            </li>
-            Or
-            <li>
-              {question.optionTwo.text}
+    <Item.Content>
+      <Item.Header as="a">
+        {
+          user.name
+        }
+        , asks Would You Rather &#63;
+      </Item.Header>
+      <Item.Description>
+        <ul>
+          <li>
+            {question.optionOne.text}
+          </li>
+          Or
+          <li>
+            {question.optionTwo.text}
 
-            </li>
-          </ul>
-        </Item.Description>
-        <Link to={`questions/${question.id}`}>
-          <Button>
-            View Details
-          </Button>
-        </Link>
-      </Item.Content>
-    </Item>);
-}
+          </li>
+        </ul>
+      </Item.Description>
+      <Link to={`questions/${question.id}`}>
+        <Button>
+          View Details
+        </Button>
+      </Link>
+    </Item.Content>
+  </Item>
+);
 
 export default QuestionCard;
 export {
   QuestionCard,
-}
+};
