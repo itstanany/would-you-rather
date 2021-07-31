78b75415beabeba56c8e52d32f4c45900356e1f8
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2021-07-31 17:08:34 +0200

    footer component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,24 @@
-const Footer = () => <p>Footer Placeholder</p>
+/**
+ * Footer component
+ * It represents static footer with a link to project repository
+ */
+import './footer.css';
+
+const Footer = () => (
+  <div className="footer">
+    <small>
+      Full Source Code of This Project Con be found at
+      {' '}
+      {' '}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://github.com/ahmedalima/"
+      >
+        Here
+      </a>
+    </small>
+  </div>
+);
 
 export default Footer;
-
-export {
-  Footer
-}
