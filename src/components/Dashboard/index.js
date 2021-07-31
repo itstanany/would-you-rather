b4490e8c511a38cee5b19318c24bd0a72cefb165
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2021-07-31 16:46:15 +0200

    debug remove unused import

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,6 +1,42 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Label, Menu, Tab } from 'semantic-ui-react'
+import { getAllQ } from '../../store';
+import { sortedQSelector } from '../../utils';
+import QuestionCard from '../QuestionsView';
+
 const Dashboard = () => {
+  const dispatch = useDispatch();
+  const { answered, unAnswered } = useSelector(sortedQSelector)
+  useEffect(() => {
+    dispatch(getAllQ());
+  }, [dispatch]);
+
+  const panes = [
+    {
+      menuItem: (
+        <Menu.Item key="unAnswered">
+          unAnswered <Label>{unAnswered.length}</Label>
+        </Menu.Item>
+      ),
+      render: () => <Tab.Pane key="unAnswered">
+        <QuestionCard qIds={unAnswered} />
+      </Tab.Pane>,
+    },
+    {
+      menuItem: (
+        <Menu.Item key='answered'>
+          Answered <Label>{answered.length}</Label>
+        </Menu.Item>
+      ),
+      render: () => <Tab.Pane>
+        <QuestionCard qIds={answered} />
+      </Tab.Pane>,
+    },
+  ]
+
   return (
-    <h1>How on earth?</h1>
+    <Tab panes={panes} />
   )
 };
 
