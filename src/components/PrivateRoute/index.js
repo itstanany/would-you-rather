119a3425d4a6f56b58bbd76927ceaae1da8f8f1f
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2021-07-31 17:28:48 +0200

    refactor eslint correction

--- src/components/PrivateRoute/index.js
+++ src/components/PrivateRoute/index.js
@@ -1,17 +1,20 @@
-import { Redirect, Route } from "react-router-dom";
-import { isLoggedIn } from "./utils"
+/* eslint-disable react/destructuring-assignment */
+import { Redirect, Route } from 'react-router-dom';
+import { isLoggedIn } from './utils';
 
 const PrivateRoute = (props) => {
   const condition = isLoggedIn();
   return (
     condition
-      ? (<Route path={props.path} component={props.component}>
-        {props.children}
-      </Route>)
+      ? (
+        <Route path={props.path} component={props.component}>
+          {props.children}
+        </Route>
+      )
       : <Redirect to={`/login?from=${props.location.pathname}`} />
-  )
-}
+  );
+};
 export default PrivateRoute;
 export {
-  PrivateRoute
-}
+  PrivateRoute,
+};
